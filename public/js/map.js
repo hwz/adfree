61a681f18626ed698c002179d77a27edeca70f14
Felix Rieseberg <felix.rieseberg@microsoft.com>
2013-09-07 22:27:31 -0700

    jquery fix

--- public/js/map.js
+++ public/js/map.js
@@ -4,6 +4,47 @@ var jobsdata = new Array();
 jobsdata.done = randomRange(400, 800);
 jobsdata.current = 0;
 
+var n = 40,
+    random = d3.random.normal(0, 20),
+    data = d3.range(n).map(random);
+ 
+var margin = {top: 20, right: 20, bottom: 20, left: 20},
+    width = 300 - margin.left - margin.right,
+    height = 200 - margin.top - margin.bottom;
+
+var x = d3.scale.linear().domain([1, n - 2]).range([0, width]);
+var y = d3.scale.linear().domain([0, 25]).range([height, 0]);
+
+var line = d3.svg.line()
+    .interpolate("basis")
+    .x(function(d, i) { return x(i); })
+    .y(function(d, i) { return y(d); });
+ 
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .attr("id", "linearGraphJobs")
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+ 
+svg.append("defs").append("clipPath")
+    .attr("id", "clip")
+    .append("rect")
+    .attr("width", width)
+    .attr("height", height);
+ 
+svg.append("g")
+    .attr("class", "y axis")
+    .attr("style", "fill: #f3de70; outline: #ffb310")
+    .call(d3.svg.axis().scale(y).orient("left"));
+ 
+var path = svg.append("g")
+    .attr("clip-path", "url(#clip)")
+    .append("path")
+    .datum(data)
+    .attr("class", "line")
+    .attr("d", line);
+
 function dataCenterLocation(x, y) {
     this.x = x;
     this.y = y;
@@ -102,55 +143,9 @@ function init() {
     }
     animateStroke(400);
     circleMagic();
+    tick();
 }
 
-init();
-
-////
-
-var n = 40,
-    random = d3.random.normal(0, 20),
-    data = d3.range(n).map(random);
- 
-var margin = {top: 20, right: 20, bottom: 20, left: 20},
-    width = 300 - margin.left - margin.right,
-    height = 200 - margin.top - margin.bottom;
- 
-var x = d3.scale.linear().domain([1, n - 2]).range([0, width]);
-var y = d3.scale.linear().domain([0, 25]).range([height, 0]);
- 
-var line = d3.svg.line()
-    .interpolate("basis")
-    .x(function(d, i) { return x(i); })
-    .y(function(d, i) { return y(d); });
- 
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .attr("id", "linearGraphJobs")
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
- 
-svg.append("defs").append("clipPath")
-    .attr("id", "clip")
-  .append("rect")
-    .attr("width", width)
-    .attr("height", height);
- 
-svg.append("g")
-    .attr("class", "y axis")
-    .attr("style", "fill: #f3de70; outline: #ffb310")
-    .call(d3.svg.axis().scale(y).orient("left"));
- 
-var path = svg.append("g")
-    .attr("clip-path", "url(#clip)")
-  .append("path")
-    .datum(data)
-    .attr("class", "line")
-    .attr("d", line);
- 
-tick();
- 
 function tick() {
  
   // push a new data point onto the back
@@ -170,3 +165,5 @@ function tick() {
   data.shift();
  
 }
+
+init();
